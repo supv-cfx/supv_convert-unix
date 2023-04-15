c49c4ca01139dabe5b8370cfcedefae15f582775
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-15 23:13:23 +0200

    init: version 1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games {"gta5", "rdr3"}
+lua54 'yes'
+
+author 'SUP2Ak#3755'
+version '1.0'
+description 'Thanks to project error for boilerplate svelte'
+
+ui_page 'web/build/index.html'
+
+client_script "client/main.lua"
+
+files {
+  'web/build/index.html',
+  'web/build/**/*'
+}
